b84a59f83195f632bde1170a4e1d5738d6939f04
jmlevy91@gmail.com <jmlevy91@gmail.com>
2018-06-13 15:51:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var name = "Joe"
+var height = "74"
+
+
+
+module.exports = {
+  name, height
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+var index = 
+  require("./index.js") 
+  
+  console.log(index.name)
+  
+if (name === "Joe") {
+  console.log("The name is correct")
+} 
